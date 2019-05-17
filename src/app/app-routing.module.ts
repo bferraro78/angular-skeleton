797bc5249a90379ca0099bbb5d4219b981425d51
Ben Ferraro <bferraro@Bens-Machine-2.local>
2019-05-16 23:36:45 -0400

    First commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LayoutComponent } from './layout/layout.component';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: '', component: LayoutComponent },
+{ path: '',
+    redirectTo: '',
+    pathMatch: 'full'
+  }];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { enableTracing: true })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
